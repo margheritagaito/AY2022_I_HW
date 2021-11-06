0316b79ac9336b7af0ca94510c999abc30da499b
Daniele Saporito <daniele.saporito@mail.polimi.it>
2021-11-06 17:39:40 +0100

    modifica main.c

--- GROUP_08.cydsn/main.c
+++ GROUP_08.cydsn/main.c
@@ -16,6 +16,8 @@ int main(void)
     CyGlobalIntEnable; /* Enable global interrupts. */
 
     /* Place your initialization/startup code here (e.g. MyInst_Start()) */
+    
+    //Ciao Margherita
 
     for(;;)
     {
