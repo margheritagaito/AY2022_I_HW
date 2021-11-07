c1241074fea985bd727e819f52858523a5bf5af3
Daniele Saporito <daniele.saporito@mail.polimi.it>
2021-11-07 18:15:23 +0100

    cartella interrupt

---
+++ InterruptRoutine/InterruptRoutine.h
@@ -0,0 +1,37 @@
+/* ========================================
+ *
+ * Copyright YOUR COMPANY, THE YEAR
+ * All Rights Reserved
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ *
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF your company.
+ *
+ * ========================================
+*/
+
+#ifndef __INTERRUPT_ROUTINE_H
+    
+    #define __INTERRUPT_ROUTINE_H
+    
+    #include "cytypes.h"
+    #include "stdio.h"
+    
+    /* **** DEFINE **** */
+    
+    
+    #define TRANSMIT_BUFFER_SIZE 16 // Credo vada modificata per l'I2C
+    #define MIN_VALUE 0
+    #define MAX_VALUE 65535
+    #define SIZE 5
+    #define LDR_CHANNEL 0
+    #define TS_CHANNEL 1
+    
+    CY_ISR_PROTO (Custom_ISR_ADC);
+    
+    char DataBuffer[TRANSMIT_BUFFER_SIZE];
+    volatile uint8 PacketReadyFlag;
+    
+#endif
+
+/* [] END OF FILE */
